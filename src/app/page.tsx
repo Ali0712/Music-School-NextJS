ff6f4120659fc93919cb09aac5f03fb043f3855a
Muhammad Ali <muhammadali07122@gmail.com>
2024-05-03 20:28:20 +0500

    chore: Update dependencies and add MusicSchoolTestimonials component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <div>
       <HeroSection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
+      <MusicSchoolTestimonials/>
     </div>
   );
 }
