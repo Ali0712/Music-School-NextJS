57c6ac6405dacbe999d63b217bab4d14dddf8c60
Muhammad Ali <muhammadali07122@gmail.com>
2024-05-04 11:29:39 +0500

    feat: Add meteor effect to background beams on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { FormEvent, useState } from 'react';
 import { BackgroundBeams } from '@/components/ui/background-beams';
+import { Meteors } from "@/components/ui/meteors";
 
 function MusicSchoolContactUs() {
   const [email, setEmail] = useState('');
@@ -19,6 +20,7 @@ function MusicSchoolContactUs() {
       {/* BackgroundBeams with adjusted z-index */}
       <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
       {/* Content with higher z-index */}
+      {/* <Meteors number={20} className="top-0 left-0 z-0 w-auto h-auto"/> */}
       <div className="max-w-2xl mx-auto p-4 relative z-10">
         {' '}
         {/* Add relative and z-10 to bring content to the front */}
